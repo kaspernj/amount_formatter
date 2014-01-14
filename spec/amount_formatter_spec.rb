b78a1df32ba99a003cbeb9b359dcfd8877dd9705
Kasper Johansen <k@spernj.org>
2014-01-14 10:07:02 +0100

    Initial work on the amount formatter.

--- spec/amount_formatter_spec.rb
+++ spec/amount_formatter_spec.rb
@@ -1,7 +1,8 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require_relative "spec_helper"
 
-describe "AmountFormatter" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe AmountFormatter do
+  it "formats numbers" do
+    AmountFormatter.format(12345.12, :precision => 2, :seperator => ",", :delimiter => ".").should eq "12.345,12"
+    AmountFormatter.format(12345.12).should eq "12,345.12"
   end
 end
