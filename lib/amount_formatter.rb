d773bd27ef0a49629d82066a7cf5ca02b36455c4
kaspernj <k@spernj.org>
2014-11-05 11:43:09 +0100

    Added I18n support.

--- lib/amount_formatter.rb
+++ lib/amount_formatter.rb
@@ -1,27 +1,55 @@
 class AmountFormatter
-  FORMAT_DEFAULT_ARGS = {:precision => 2, :separator => ".", :delimiter => ","}
-  
+  FORMAT_DEFAULT_ARGS = {precision: 2}
+
+  def self.locale_engine=(engine)
+    @locale_engine = engine
+  end
+
   #Returns the number as a formatted string.
   def self.format(number, args = {})
+    separator = args[:separator] || default_separator
+    delimiter = args[:delimiter] || default_delimiter
+
     args = FORMAT_DEFAULT_ARGS.merge(args)
     number = number.to_f unless number.is_a?(Float)
-    return sprintf("%.#{args[:precision].to_i}f", number).gsub(".", args[:separator]) if number < 1 && number > -1
+    return sprintf("%.#{args[:precision].to_i}f", number).gsub(".", separator) if number < 1 && number > -1
     number = sprintf("%.#{args[:precision].to_i}f", number).split(".")
-    
+
     str = ""
     number[0].reverse.scan(/(.{1,3})/) do |match|
       if match[0] == "-"
         #This happens if the number is a negative number and we reach the minus-sign.
         str << match[0]
       else
-        str << args[:delimiter] if str.length > 0
+        str << delimiter if str.length > 0
         str << match[0]
       end
     end
-    
+
     str = str.reverse
-    str << "#{args[:separator]}#{number[1]}" if args[:precision] > 0
-    
+    str << "#{separator}#{number[1]}" if args[:precision] > 0
+
     return str
+  end
+
+private
+
+  def self.default_separator
+    if @locale_engine == :i18n && I18n.exists?("number.format.separator")
+      translation = I18n.t("number.format.separator").to_s.strip
+      return translation unless translation.empty?
+      I18n.t("number.format.separator")
+    end
+
+    return "."
+  end
+
+  def self.default_delimiter
+    if @locale_engine == :i18n && I18n.exists?("number.format.delimiter")
+      translation = I18n.t("number.format.delimiter").to_s.strip
+      return translation unless translation.empty?
+    end
+
+    return ","
   end
 end
